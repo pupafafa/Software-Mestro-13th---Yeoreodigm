b98b4467d2a9ce52d7a62d53ed5461a6c223b82b
pupafafa <wishluz1997@gmail.com>
2022-08-10 22:51:40 +0900

    [#none] fix bug of recommend_place function in place_lambda_function

--- code_files/place_lambda_function.py
+++ code_files/place_lambda_function.py
@@ -60,6 +60,70 @@ def load_place(db):
   df.columns = ['place_id','nature','outdoor','fatigue','sea','walking','exciting','day','culture','cluster']
   df = df.sort_values(by='place_id',ascending=True)
   return df
+
+import json
+import pandas as pd
+import psycopg2
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import random
+import os
+
+def load_db():
+  endpoint = os.environ['END_POINT']
+  dbname = os.environ['DB_NAME']
+  user = os.environ['USER_NAME']
+  password = os.environ['WONSEOK']
+  db = psycopg2.connect(host=endpoint,dbname=dbname,user=user,password=password)
+  return db
+
+def find_places_in_travel_note(travel_note_id, db):
+  cursor= db.cursor()
+  sql = f"select places from course where travel_note_id={travel_note_id}"
+  cursor.execute(sql)
+  result = cursor.fetchall()
+  for places in result:
+    print(places)
+  places = []
+  for path in result:
+    places += path[0]
+  return places
+  
+def calculate_taste(user_id,db):
+  print("calculate_tast...")
+  cursor = db.cursor()
+  load_survey_sql = f'SELECT result FROM survey_result WHERE member_id = {user_id};'
+  cursor.execute(load_survey_sql)
+  result = cursor.fetchone()
+  survey_result = result[0]
+  print(survey_result)
+
+  theme_score = np.array([0]*8)
+  for place in survey_result:
+    load_place_sql = f'SELECT nature,outdoor,fatigue,sea,walking,exciting,day,culture FROM places_analysis WHERE place_id={place}'
+    cursor.execute(load_place_sql)
+    place_theme = cursor.fetchone()
+    
+    theme_score += np.array(place_theme)
+    #print('place : ',place,"\tnow : ",place_theme)
+
+  avg_theme_score = theme_score / len(survey_result)
+  user_taste = pd.DataFrame(avg_theme_score.reshape(1,-1))
+  user_taste.columns = ['nature','outdoor','fatigue','sea','walking','exciting','day','culture']
+  print("calculate_tast DONE!!")
+  return user_taste
+
+def load_place(db):
+  cursor= db.cursor()
+  
+  load_place_sql= f'SELECT place_id,nature,outdoor,fatigue,sea,walking,exciting,day,culture,cluster FROM places_analysis'
+  cursor.execute(load_place_sql)
+  result = cursor.fetchall()
+  df = pd.DataFrame(result)
+  df.columns = ['place_id','nature','outdoor','fatigue','sea','walking','exciting','day','culture','cluster']
+  df = df.sort_values(by='place_id',ascending=True)
+  return df
+
 def recommend_place(user_id,travel_note_id):
   
   top = 50
@@ -94,13 +158,15 @@ def recommend_place(user_id,travel_note_id):
   
   result = place.iloc[top_N_result].sort_values(by='place_id',ascending=True)
   result = result.iloc[:top]
-  print("result.iloc[:top]")
+  print("***************************************result***************************************")
   print(result)
 
+  #
+  result_copy = result.copy() #다시 뽑을 경우를 대비해서 원본 복사본 만들어놓기.
   result = random.sample(list(result['place_id']), 2) 
   # if course에 있는거랑 곂치면 다시 뽑자!!!
   while set(result).intersection(places_in_course) != set():
-    result = random.sample(list(result['place_id']), 2)
+    result = random.sample(list(result_copy['place_id']), 2)
 
   places_in_course += result
 
@@ -141,15 +207,42 @@ def recommend_place(user_id,travel_note_id):
   result_six = result_six.iloc[:top]
   print("result _ six")
   print(result_six)
-  
+  result_six_copy = result_six.copy() #다시 뽑을 경우를 대비해서 원본 복사본 만들어놓기.
   result_six = random.sample(list(result_six['place_id']), 2) # if 위에서 뽑은거랑 곂치면 다시 뽑자!!!
   while set(result_six).intersection(places_in_course) != set():
-    result_six = random.sample(list(result_six['place_id']), 2)
+    result_six = random.sample(list(result_six_copy['place_id']), 2)
 
   
   final_result = result+result_six
 
   return final_result
+
+
+def lambda_handler(event, context):
+  # TODO implement
+  id = int(event['queryStringParameters']['memberId'])
+  travel_note_id = int(event['queryStringParameters']['travelNoteId'])
+  try:
+    db = load_db()
+  except:
+    return {
+            'statusCode': 500,
+            "success": False,
+            "message": "Database Error",
+            
+            
+        }
+        
+  place_list = recommend_place(id,travel_note_id)
+  
+  
+
+  return {
+      'statusCode': 200,
+      'body': json.dumps({  
+                            "placeList" : place_list,
+                            })
+  }
 
 
 def lambda_handler(event, context):
