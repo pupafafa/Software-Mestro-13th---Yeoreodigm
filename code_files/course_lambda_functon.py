018eff02f38703c7e6863a4e1aee652aac8f127c
pupafafa <wishluz1997@gmail.com>
2022-08-10 22:44:54 +0900

    [#none] lambda_function upload

---
+++ code_files/course_lambda_functon.py
@@ -0,0 +1,206 @@
+import json
+import pandas as pd
+import psycopg2
+import numpy as np
+import os
+from sklearn.metrics.pairwise import cosine_similarity
+from itertools import combinations
+
+def load_db():
+  endpoint = os.environ['END_POINT']
+  dbname = os.environ['DB_NAME']
+  user = os.environ['USER_NAME']
+  password = os.environ['WONSEOK']
+  db = psycopg2.connect(host=endpoint,dbname=dbname,user=user,password=password)
+  return db
+
+def calculate_taste(user_id,db):
+  cursor = db.cursor()
+  load_survey_sql = f'SELECT result FROM survey_result WHERE member_id = {user_id};'
+  cursor.execute(load_survey_sql)
+  result = cursor.fetchone()
+  survey_result = result[0]
+  print(survey_result)
+
+  theme_score = np.array([0]*8)
+  for place in survey_result:
+    load_place_sql = f'SELECT nature,outdoor,fatigue,sea,walking,exciting,day,culture FROM places_analysis WHERE place_id={place}'
+    cursor.execute(load_place_sql)
+    place_theme = cursor.fetchone()
+    
+    theme_score += np.array(place_theme)
+    print('place : ',place,"\tnow : ",place_theme)
+
+  result = theme_score / len(survey_result)
+  return result
+
+
+def path_divider(day,path):
+  path_per_day = []
+  checker = day
+
+  length = len(path)
+  print('length :',length )
+
+  remainder = length%day
+  quotient = length//day
+
+  start = 0
+  check=0
+
+  while start<length:
+    print(path[start:start + quotient + check])
+    path_per_day.append(path[start:start + quotient + check])
+    start += quotient+check
+    checker-=1
+    if checker<=remainder:
+      check = 1
+
+  return path_per_day
+
+def load_course(day,db,include,location):
+  if day>=25:
+    day = 25
+    location = []
+  cursor= db.cursor()
+  min_length = 2*day
+  max_length = 4*day
+  length = len(include)
+  count = 0
+  location_count = len(location)
+  if location_count == 4 or location_count == 0:
+    location_sql = ' and 1=1'
+  #case 2 : 한 지역만 선택한 경우 
+  elif location_count == 1:
+    if day>=10:
+      location_sql = ' and 1=1'
+    else:
+      location_sql = f" and LOWER(main_location) = \'{location[0]}\'"
+  #case 3 : 여러 지역을 선택한 경우
+  else:
+    if location_count == 2:
+      location_sql = f" and main_location = 'Mixed' and {location[0]} >0 and {location[1]} >0"
+    elif location_count == 3:
+      location_sql = f" and main_location = 'Mixed' and {location[0]} >0 and {location[1]} >0 and {location[2]} >0"
+
+
+  df = pd.DataFrame()
+  while length >= count:
+    possible_cases = list(combinations(include,length-count))
+    ##sql query 날리고 결과 받아서
+    for now in possible_cases:
+      print("now : ",list(now))
+      now_include = list(now)
+      load_course_sql= (f"SELECT id,title,nature,outdoor,fatigue,sea,walking,exciting,day,culture,cluster,places, main_location\
+ FROM course_ WHERE length>={min_length} and length<={max_length} and array{now_include}::smallint[] <@ places" + location_sql)
+      #해당 query문으로 query 날려서 결과가 있는지 확인해야함.
+      
+      cursor.execute(load_course_sql)
+      result = cursor.fetchall()
+
+      #현재 포함시킬 place를 모두 가지는 코스가 없는경우 
+      if len(result) == 0:
+        continue
+      #현재 Place를 모두 포함하는 코스가 있는경우
+      else:
+        print("현재 now를 포함하는 df : \n",pd.DataFrame(result),"\n****************************\n")
+        df = pd.concat([df, pd.DataFrame(result)])
+    
+    #dataframe에 값이 존재하면 (최적의 코스를 찾았다면)
+    if not df.empty:
+      break    
+    count += 1
+  
+  #해당 조건에 만족하는 코스가 없는 경우
+  if df.empty:
+    print("no course")
+    return df
+
+  df.columns = ['id','title','nature','outdoor','fatigue','sea','walking','exciting','day','culture','cluster','places','main_location']
+
+  return df
+  #intersect 밖의 place를 모든 df의 Places list에 append 해줘야함.
+
+
+
+def recommend_by_theme(user_id, day, include_list,location,db):
+  top = 5
+  course = load_course(day,db,include_list,location)
+  if course.empty:
+    print("empty course")
+    return None
+
+  user_taste = calculate_taste(user_id,db)
+  user_taste = user_taste.reshape(1,-1)
+
+  tag = ['nature','outdoor','fatigue','sea','walking','exciting','day','culture']
+
+  print(user_taste)
+  
+  similarity = cosine_similarity(user_taste, course[tag])
+  similarity = similarity[0]
+  print(similarity.shape)
+      
+  print('similarity : ',similarity)
+  
+  result_index = similarity.argsort()
+  result_index = result_index
+  
+  print("result_index : ",result_index)
+
+  for i in range(-1,-(len(result_index)+1),-1):
+    now_idx = result_index[i]
+    sim = similarity[now_idx]
+    print("similarity : ",sim,"df index : ",now_idx)
+
+  top_N_result = result_index[-1:-(top+1):-1]
+  print("top_N_result",top_N_result)
+  
+  #결과는 가장 일치율이 높은거 1개의 index만 던져주자
+  result = top_N_result[0]
+  result_df = course.iloc[result]
+  print(result_df)
+  
+  not_included_places = list(set(include_list) - set(result_df['places']))
+  if 0 not in include_list:
+    result_df['places'] += not_included_places
+  
+  path = path_divider(day,result_df['places'])
+  print('path : ',path)
+  return path
+    
+def to_lower_case(string):
+  return string.lower()
+    
+def lambda_handler(event, context):
+
+  id = int(event['queryStringParameters']['id'])
+  day = int(event['queryStringParameters']['day'])
+  include = list(map(int,event['queryStringParameters']['include'].split(',')))
+  location = event['queryStringParameters']['location'].split(',')
+  location = list(map(to_lower_case, location) )
+  try:
+    db = load_db()
+  except:
+        return {
+            "success": False,
+            "message": "Database Error",
+        }
+  result_path = recommend_by_theme(id, day,include,location,db)
+  if result_path == None:
+    return {
+      'statusCode': 200,
+      'body': json.dumps({  
+                            "courseList" : "[No corresponding course]",
+                            })
+  }
+  
+  
+
+  return {
+      'statusCode': 200,
+      'body': json.dumps({  
+                            "courseList" : result_path,
+                            })
+  }
+  
---
+++ code_files/place_lambda_function.py
@@ -0,0 +1,179 @@
+import json
+import pandas as pd
+import psycopg2
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import random
+import os
+
+def load_db():
+  endpoint = os.environ['END_POINT']
+  dbname = os.environ['DB_NAME']
+  user = os.environ['USER_NAME']
+  password = os.environ['WONSEOK']
+  db = psycopg2.connect(host=endpoint,dbname=dbname,user=user,password=password)
+  return db
+
+def find_places_in_travel_note(travel_note_id, db):
+  cursor= db.cursor()
+  sql = f"select places from course where travel_note_id={travel_note_id}"
+  cursor.execute(sql)
+  result = cursor.fetchall()
+  for places in result:
+    print(places)
+  places = []
+  for path in result:
+    places += path[0]
+  return places
+  
+def calculate_taste(user_id,db):
+  print("calculate_tast...")
+  cursor = db.cursor()
+  load_survey_sql = f'SELECT result FROM survey_result WHERE member_id = {user_id};'
+  cursor.execute(load_survey_sql)
+  result = cursor.fetchone()
+  survey_result = result[0]
+  print(survey_result)
+
+  theme_score = np.array([0]*8)
+  for place in survey_result:
+    load_place_sql = f'SELECT nature,outdoor,fatigue,sea,walking,exciting,day,culture FROM places_analysis WHERE place_id={place}'
+    cursor.execute(load_place_sql)
+    place_theme = cursor.fetchone()
+    
+    theme_score += np.array(place_theme)
+    #print('place : ',place,"\tnow : ",place_theme)
+
+  avg_theme_score = theme_score / len(survey_result)
+  user_taste = pd.DataFrame(avg_theme_score.reshape(1,-1))
+  user_taste.columns = ['nature','outdoor','fatigue','sea','walking','exciting','day','culture']
+  print("calculate_tast DONE!!")
+  return user_taste
+
+def load_place(db):
+  cursor= db.cursor()
+  
+  load_place_sql= f'SELECT place_id,nature,outdoor,fatigue,sea,walking,exciting,day,culture,cluster FROM places_analysis'
+  cursor.execute(load_place_sql)
+  result = cursor.fetchall()
+  df = pd.DataFrame(result)
+  df.columns = ['place_id','nature','outdoor','fatigue','sea','walking','exciting','day','culture','cluster']
+  df = df.sort_values(by='place_id',ascending=True)
+  return df
+def recommend_place(user_id,travel_note_id):
+  
+  top = 50
+  db = load_db()
+  place = load_place(db)
+  user_taste = calculate_taste(user_id,db)
+  places_in_course = find_places_in_travel_note(travel_note_id, db)
+  tag = ['nature','outdoor','fatigue','sea','walking','exciting','day','culture']
+  
+
+  #entire tag
+  print(user_taste)
+  entire_similarity = cosine_similarity(user_taste, place[tag])
+  entire_similarity = entire_similarity[0]
+      
+  print('entire similarity : ',entire_similarity)
+  
+  result_index = entire_similarity.argsort()
+  
+  print("result_index : ",result_index)
+
+
+  for i in range(-1,-4,-1):
+    now_idx = result_index[i]
+    sim = entire_similarity[now_idx]
+    print("now place ",place.iloc[now_idx])
+    print("similarity : ",sim,"df index : ",now_idx)
+    
+
+  top_N_result = result_index[-1:-(101):-1]
+  print("top_N_similart_result",top_N_result[:10])
+  
+  result = place.iloc[top_N_result].sort_values(by='place_id',ascending=True)
+  result = result.iloc[:top]
+  print("result.iloc[:top]")
+  print(result)
+
+  result = random.sample(list(result['place_id']), 2) 
+  # if course에 있는거랑 곂치면 다시 뽑자!!!
+  while set(result).intersection(places_in_course) != set():
+    result = random.sample(list(result['place_id']), 2)
+
+  places_in_course += result
+
+  
+  print("result **id** list",result)
+  
+  print("*"*50)
+
+  #6개 태그만 적용하기
+  #2개를 뽑아서 없앤다음, 적용하자.4개중 nature, exciting, culture, day
+  six_tag = tag.copy()
+  picking_list = ['nature', 'exciting', 'sea', 'day']
+  randomly_chosen_two_tags = random.sample(picking_list,2)
+  for tag in randomly_chosen_two_tags:
+    six_tag.remove(tag) #뽑힌 2개는 지워주기
+
+  user_taste_six = user_taste[six_tag]
+  print("user taste six : ",user_taste_six)
+  six_tag_similarity = cosine_similarity(user_taste_six, place[six_tag])
+  six_tag_similarity = six_tag_similarity[0]
+  
+  print('six similarity : ',six_tag_similarity)
+  
+  six_result_index = six_tag_similarity.argsort()
+  
+  print("six result_index : ",six_result_index)
+
+  for i in range(-1,-4,-1):
+    now_idx = six_result_index[i]
+    sim = six_tag_similarity[now_idx]
+    print("now place ",place.iloc[now_idx])
+    print("similarity : ",sim,"df index : ",now_idx)
+    
+  top_N_result_six = six_result_index[-1:-101:-1]
+  print("top_N_result(only six tag)",top_N_result_six[:10])
+  
+  result_six = place.iloc[top_N_result_six].sort_values(by='place_id',ascending=True)
+  result_six = result_six.iloc[:top]
+  print("result _ six")
+  print(result_six)
+  
+  result_six = random.sample(list(result_six['place_id']), 2) # if 위에서 뽑은거랑 곂치면 다시 뽑자!!!
+  while set(result_six).intersection(places_in_course) != set():
+    result_six = random.sample(list(result_six['place_id']), 2)
+
+  
+  final_result = result+result_six
+
+  return final_result
+
+
+def lambda_handler(event, context):
+  # TODO implement
+  id = int(event['queryStringParameters']['memberId'])
+  travel_note_id = int(event['queryStringParameters']['travelNoteId'])
+  try:
+    db = load_db()
+  except:
+    return {
+            'statusCode': 500,
+            "success": False,
+            "message": "Database Error",
+            
+            
+        }
+        
+  place_list = recommend_place(id,travel_note_id)
+  
+  
+
+  return {
+      'statusCode': 200,
+      'body': json.dumps({  
+                            "placeList" : place_list,
+                            })
+  }
