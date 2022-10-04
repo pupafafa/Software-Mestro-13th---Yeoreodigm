1413b24f1dfcc9fe9888adb8fb8a77c49c0b02cd
pupafafa <wishluz1997@gmail.com>
2022-10-04 15:13:43 +0900

    [#80] 포토다임 lambda함수 상태코드 400 추가

--- code_files/aws lambda/makePhotodigm_lambda_function.py
+++ code_files/aws lambda/makePhotodigm_lambda_function.py
@@ -11,10 +11,13 @@ prefix_info = {
   'photoDigm' : "assets/photoDigm/photoDigm/",
 }
 
+frame_coordinate = {1:(33,323,110,473),2:(33,323,110,473),3:(47,652,101,516),4:(47,652,101,516)}
+frame_inner_size = {1: (261,340),2:(261,340),3:(509,407),4:(509,407)}
+
 def connect_s3():
-  return boto3.client('s3',aws_access_key_id="AKIA3K5IYMAM2GOIJ3FL",
-                                  aws_secret_access_key="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",
-                                  region_name="ap-northeast-2")
+  return boto3.client('s3',aws_access_key_id=os.environ['ACCESS_KEY_ID'],
+                                  aws_secret_access_key=os.environ['SECRET_ACCESS_KEY'],
+                                  region_name=os.environ['REGION'])
     
 #img_type : frame,image,photoDigm
 def load_image(client,img_type,file_name):
@@ -34,49 +37,51 @@ def load_image(client,img_type,file_name):
     
     
 def lambda_handler(event, context):
-  # TODO implement
-  img1 = event['img1']
-  img2 = event['img2']
-  img3 = event['img3']
-  img4 = event['img4']
-  frame = event['frame']
-  key = event['key']
-  
-  client = connect_s3()
-  
   try:
+    # TODO implement
+    img1 = event['img1']
+    img2 = event['img2']
+    img3 = event['img3']
+    img4 = event['img4']
+    frame = event['frame']
+    key = event['key']
+    print(event)
+    print(context)
+    
+    frame_num = int(frame[5:6])
+  
+    x1,x2,y1,y2 = frame_coordinate[frame_num]
+    inner_size = frame_inner_size[frame_num]
+  
+    
+    client = connect_s3()
+    print("client connection success!!")
+  
     frame_img = load_image(client=client,img_type='frame',file_name=frame) 
-    img1 = load_image(client=client,img_type='image',file_name=img1).resize((260,340))
-    img2 = load_image(client=client,img_type='image',file_name=img2).resize((260,340))
-    img3 = load_image(client=client,img_type='image',file_name=img3).resize((260,340))
-    img4 = load_image(client=client,img_type='image',file_name=img4).resize((260,340))
+    img1 = load_image(client=client,img_type='image',file_name=img1).resize(inner_size)
+    img2 = load_image(client=client,img_type='image',file_name=img2).resize(inner_size)
+    img3 = load_image(client=client,img_type='image',file_name=img3).resize(inner_size)
+    img4 = load_image(client=client,img_type='image',file_name=img4).resize(inner_size)
+    
+    print("loaded image files successfully")
+    
+    frame_img.paste(img1,(x1,y1))
+    frame_img.paste(img2,(x2,y1))
+    frame_img.paste(img3,(x1,y2))
+    frame_img.paste(img4,(x2,y2))
+    
+    mem_out = io.BytesIO()
+    frame_img.save(mem_out,format=frame_img.format) # binary data를 BytesIO로 변환해서 memout에 담기
+    mem_out.seek(0)#파일포인터(?) 복구
+    bucket_name = 'yeoreodigm-s3'
+    save_path = prefix_info['photoDigm'] + key
+    
+    client.upload_fileobj(mem_out,bucket_name,save_path,ExtraArgs={'ContentType':"image/png"})
+    
+      
+    print("upload done")
+  
+    
   except:
-      return {
-      'statusCode': 400,
-      'body': json.dumps('Failed to Load Image from s3!'),
-
-  }
-  x1 = 34
-  x2 = 322
-  y1 = 110
-  y2 = 472
-  
-  frame_img.paste(img1,(x1,y1))
-  frame_img.paste(img2,(x2,y1))
-  frame_img.paste(img3,(x1,y2))
-  frame_img.paste(img4,(x2,y2))
-  
-  mem_out = io.BytesIO()
-  frame_img.save(mem_out,format=frame_img.format) # binary data를 BytesIO로 변환해서 memout에 담기
-  mem_out.seek(0)#파일포인터(?) 복구
-  bucket_name = 'yeoreodigm-s3'
-  save_path = prefix_info['photoDigm'] + key
-  client.upload_fileobj(mem_out,bucket_name,save_path)
-  
-  return {
-      'statusCode': 200,
-      'body': json.dumps('Hello from Lambda!'),
-      
-      
-      
-  }
+    print("except!!! ")
+    raise Exception('Invalid file path ...')
